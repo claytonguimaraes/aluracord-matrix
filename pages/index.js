d1d9603ae5ded37e306a2ee7dd2e394a3769d264
Clayton Almeida <clayton.almeida@vetta.digital>
2022-01-25 13:35:09 -0300

    feat: add states and router

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import appConfig from '../config.json';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-          * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            list-style: none;
-          }
-          body {
-            font-family: 'Open Sans', sans-serif;
-          }
-          /* App fit Height */ 
-          html, body, #__next {
-            min-height: 100vh;
-            display: flex;
-            flex: 1;
-          }
-          #__next {
-            flex: 1;
-          }
-          #__next > * {
-            flex: 1;
-          }
-          /* ./App fit Height */ 
-        `}</style>
-    );
-}
+import * as React from 'react';
+import { useRouter } from 'next/router';
 
 function Title(props) {
     console.log(props);
@@ -63,11 +36,12 @@ function Title(props) {
 // export default HomePage;
 
 export default function PaginaInicial() {
-    const username = 'peas';
+    // const username = 'peas';
+    const [username, setUsername] = React.useState('peas');
+    const roteamento = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -94,6 +68,10 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (event) {
+                            event.preventDefault();
+                            roteamento.push('/chat');
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -113,6 +91,9 @@ export default function PaginaInicial() {
                                     mainColorHighlight: appConfig.theme.colors.primary[500],
                                     backgroundColor: appConfig.theme.colors.neutrals[800],
                                 },
+                            }}
+                            onChange={function (event) {
+                                setUsername(event.target.value);
                             }}
                         />
                         <Button
